c147b64a3e24c9dea8e382603d6310f5991c2c4b
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-11-09 02:39:18 -0300

    teste dele ajustado, sem falar do script de compilação

--- testes/unit_tests_legivel.c
+++ testes/unit_tests_legivel.c
@@ -10,43 +10,50 @@
 #include "../teste.h"
 #include "../legivel.c"
 
+uint64_t entradas[] = {
+   382, 12832, 3842394, 7712340981,
+   111931512, 50123812341, 
+   100030231892377
+};
+
+double segundos[] = {
+   51.3232, 12832.15, 8328.0,
+   38832.312, 0.001, 0.038,
+   0.000851, 0.000000701, 190.5321
+};
+
 // criando hipóteses:
 void legibilidade_do_tempo() {
-   double segundos[] = {
-      51.3232, 12832.15, 8328.0,
-      38832.312, 0.001, 0.038,
-      0.000851, 0.000000701, 190.5321
-   };
-
    for(int p = 0; p < 9; p++)
       printf(
-         "%lf ===> %s\n", 
+         "%16lf ===> %s\n", 
          segundos[p], 
          tempo_legivel(segundos[p])
       );
-
    // de avaliação manual?
 }
 
 void legibilidade_de_tamanhos() {
-   uint64_t entradas[] = {
-      382, 12832, 3842394, 7712340981,
-      111931512, 50123812341, 
-      100030231892377
-   };
-
    for(int p = 0; p < 7; p++)
       printf(
-         "%lu ===> %s\n", 
+         "%16lu ===> %s\n", 
          entradas[p], 
          tamanho_legivel(entradas[p])
       );
 }
 
+void o_grosso_de_grande_valores() {
+   for(int p = 0; p < 7; p++)
+      printf(
+         "%16lu ===> %s\n", 
+         entradas[p], 
+         valor_legivel(entradas[p])
+      );
+}
+
 
 // execução de todas hipóteses dadas:
-void main(int argc, char** argv) {
-
+int main(int argc, char** argv) {
    executa_teste(
       "função de legibilidade humana do tempo",
       legibilidade_do_tempo
@@ -56,4 +63,8 @@ void main(int argc, char** argv) {
       "função de legibilidade humana do tamanho",
       legibilidade_de_tamanhos
    );
+
+   executa_testes(1, o_grosso_de_grande_valores, true);
+
+   return EXIT_SUCCESS;
 }
