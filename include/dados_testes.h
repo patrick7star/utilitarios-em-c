b0f3844aab4bb0cc1f8e2808f4078ce68386b520
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-04-06 20:44:51 -0300

    Apenas despejando, pois não sei bem especificamente...as últimas alterações feitas, onde elas foram feitas?

--- include/dados_testes.h
+++ include/dados_testes.h
@@ -17,22 +17,36 @@
 #include <stdint.h>
 
 #ifndef DADOS_TESTES_H
+#define DADOS_TESTES_H
+
+// tamanho de cada array abaixo:
+#define FRUTAS 9llu
+#define NOMES_DE_MENINOS 14llu
+#define NOMES_DE_MENINAS 15llu
+#define GIRLS_NAMES 9llu
+#define BOYS_NAMES 11llu
+#define LEGUMES 9llu
+#define SEXO 3llu
+#define VALORES_PADRONIZADOS 10llu
+#define VALORES_PADRONIZADOS_I 16llu
+#define VEICULOS 16llu
+
 
 // contei 9 frutas.
-char* frutas[] = {
+const char* frutas[] = {
    "morango", "banana", "pêra", "uva", "mamão",
    "laranja", "abacaxi", "melância", "goiaba",
 };
 
 // eu contei 15 nomes.
-char* nomes_de_meninas[] = {
+const char* nomes_de_meninas[] = {
    "Júlia", "Amanda", "Sara", "Elisa", "Mariana",
    "Gabriela", "Maria", "Joana", "Carol", "Maísa",
    "Rosana", "Larissa", "Geovana", "Alice", "Marta"
 };
 
 // Eu contei 14 nomes.
-char* nomes_de_meninos[] = {
+const char* nomes_de_meninos[] = {
    "João", "César", "Maurício", "Júlio", "Marcos",
    "Paulo", "Johnatan", "Mateus", "Pedro", "Ronaldo",
    "Romeu", "Gilberto", "Carlos", "Ivan"
@@ -50,14 +64,35 @@ char* boys_names[] = {
    "John", "Taylor", "Charlie", "Dave", "Thomas", "Jacob"
 };
 
-// eu contei 8 legumes.
+// eu contei 9 legumes.
 char* legumes[] = { 
    "batata", "tomate", "cenoura", "beterraba", "pimentão",
-   "mandioca", "alface", "cebola"
+   "mandioca", "alface", "cebola", "repolho"
 };
 
-char sexo[] = { 'F', 'M' };
+char sexo[] = { 'F', 'M', 'N'};
 
 // eu contei 10 inteiros.
 uint8_t valores_padronizados[] = { 9, 0, 8, 1, 7, 2, 6, 3, 5, 4};
+
+// eu contei 36 inteiros positivos.
+uint16_t valores_padronizados_i[] = {
+   1, 11, 111, 1111,
+   9, 99, 999, 9999, 
+   8, 88, 888, 8888,
+   7, 77, 777, 7777,
+   3, 33, 333, 3333,
+   4, 44, 444, 4444,
+   2, 22, 222, 2222,
+   5, 55, 555, 5555,
+   6, 66, 666, 6666
+};
+
+// eu contei 16 strings.
+char* veiculos[] = {
+   "bicicleta", "skate", "carro", "ônibus", 
+   "patins", "cavalo", "carroça", "escada rolante",
+   "prancha de surf", "bote", "barco", "vela", "navio",
+   "avião", "paraquedas", "helicoptero"
+};
 #endif
