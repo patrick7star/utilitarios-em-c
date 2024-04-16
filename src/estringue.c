5dfc2ab4ed44654cbf05057bbafda4bff12cad30
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-04-15 22:10:14 -0300

    utilitários de strings acrescentados.
    A biblioteca padrão que cuida disso é muito limitada.

--- src/estringue.c
+++ src/estringue.c
@@ -8,260 +8,210 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-
-const size_t INICIAL = 100;
-
-typedef struct {
-   // array que armazena os caractéres.
-   char caracteres[INICIAL];
-
-   // quantidade de caractéres que a array interna da estrutura permite.
-   size_t capacidade;
-
-   // cursor onde adicionar mais perdaços de string.
-   size_t cursor;
-} string_t, *String, *STRING;
-
-String cria_str(const char* conteudo) {
-   size_t tamanho_string = strlen(conteudo);
-   size_t capacity;
-
-   // escolher capacidade baseado se a string excede a padrão.
-   if (tamanho_string >= INICIAL)
-      capacity = tamanho_string + 1;
-   else
-      capacity = INICIAL;
-
-   // muito específico para esta máquina.
-   String s = malloc(capacity + 8 + 8);
-   // String s = malloc(sizeof(string_t));
-
-   // só faz as operações abaixo, se e somente se, ter sido alocado algo.
-   if (s == NULL) {
-      s->capacidade = capacity;
-      // preenchendo com fim's de linha.
-      memset(s->buffer, '\0', s->capacidade);
-
-      while (*conteudo) {
-         s->caracteres[s->cursor] = conteudo[0];
-         s->cursor += 1;
-         conteudo++;
-      }
-   }
-   return s;
-}
-
-String cria_vazia_str() 
-   { return cria_str(""); }
-
-void destroi_str(String s) {
-   free(s->buffer);
-   free(s);
-}
-
-size_t tamanho_str(String s) 
-   { return s->cursor; }
-
-size_t vazia_str(String s)
-   { return s->cursor == 0; }
-
-void insere_str(String s, char _char) {
-   s->buffer[s->cursor] = _char;
-   s->cursor += 1;
-}
-
-void concatena_str(String s, char* string) {
-   while (*string) { 
-      insere_str(s, string[0]); 
-      string++; 
-   }
-}
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
 
 typedef char* char_t, CHAR, Char;
 // macro para outra abordagem de retorno.
 #define CARACTERE_NAO_ENCONTRADO 10
 
-/* Primeira abordagem, retornar um endereço para o caractére removido ou
- * indexado. A segunda retorna sempre um caractére, ou o valor que o macro 
- * acima define como não encontrado/removido e etc.
- */
-char_t retira_str(String s) {
-   /* Por que não dá apenas o endereço do caractére, já que ele não é 
-    * apagada em tal operação? Bem, justamente por isso, sim ele não é 
-    * apagado na operação, porém pode ser modificado futuramente com a 
-    * expansão da array interna, ou mesmo na inserção de algo novo, lembre
-    * se, tal posição foi removida. */
-   if (vazia_str(s))
-      return NULL;
-   char* removido = malloc(1); 
-   removido = s->buffer[s->cursor];
-   s->cursor -= 1;
-   return removido;
+
+#ifdef _UT_STRING
+static void visualiza_array(size_t* array, size_t t) {
+   printf ("[");
+   for (size_t p = 1; p <= t; p++)
+      printf ("%lu, ", array[p - 1]);
+   if (t == 0)
+      puts("]");
+   else
+      puts("\b\b]");
 }
+#endif
 
-char_t get_str(String s, size_t i) {
-   if (i >= s->cursor || vazia_str(s)) 
-      // não pode exceder o 'cursor', que é a última posição.
-      return NULL;
-   /* neste caso ele retorna a um endereço, é seu trabalho copiar ou não 
-    * o caractére dado. */
-   return &s->buffer[i];
-}
-
-char retira_i_str(String s) {
-   if (vazia_str(s))
-      return CARACTERE_NAO_ENCONTRADO;
-   /* retorna uma cópia do caractére na última posição da string, esta é 
-    * a remoção. */
-   s->cursor -= 1;
-   return s->buffer[s->cursor + 1];
-}
-
-char get_i_str(String s, size_t i) {
-   if (i >= s->cursor || vazia_str(s)) 
-      // não pode exceder o 'cursor', que é a última posição.
-      return CARACTERE_NAO_ENCONTRADO;
-
-   return s->buffer[i];
-}
-
-bool set_str(String s, char ch, size_t i) {
-   if (vazia_str(s) || i >= s->cursor)
-      return false;
-   s->buffer[i] = ch;
-   // confirma operação.
-   return true;
-}
-
-void imprime_str(String s) {
-   printf("string: \'");
-   for (size_t i = 0; i <= f; i++) {
-      char* char_ = &s->buffer[i]; 
-      printf("%c", *char_);
+static char* copia_trecho (char* string, size_t i, size_t f) {
+   if (f < i) {
+      perror ("confusão nos marcos dados!");
+      abort();
    }
-   puts("\'");
+
+   // faz uma cópia do trecho específico, e a retorna.
+   char* trecho = string + i;
+   size_t L = f - i;
+   char* palavra = malloc (L * sizeof(char));
+
+   // limpando e copiando em seguida ...
+   memset (palavra, '\0', L);
+   memmove (palavra, trecho, L);
+   // retornando a cópia.
+   return palavra;
 }
 
-typedef struct {
-   // a string inteira.
-   unsigned char* string;
+typedef struct { char** lista; size_t total; } OutcomePalavras;
+typedef OutcomePalavras OutcomeStrings;
 
-   // onde nela é relevante.
-   size_t inicio; size_t final;
-} slice_t;
-
-static void imprime_slice(slice_t a) {
-   for (size_t i = a.inicio; i <= a.final; i++) {
-      // preenche espaços com pontos prá melhor visualização(debug mode).
-      #ifdef _DEBUG
-      if (a.string[i] == ' ') {
-         printf(".");
-         continue;
-      }
-      #endif
-      printf("%c", a.string[i]);
-   }
-   puts("");
-}
-
-static bool compara_slice_e_str(slice_t a, unsigned char* s) {
-   for (size_t i = 0; i < strlen(s); i++) {
-      if (s[i] != a.string[a.inicio + i])
-         return false;
-   }
-   // se chegar até aqui.
-   return true;
-}
-
-typedef struct { size_t* indices; size_t total; } indices_t;
-
-static indices_t indices_buscado(unsigned char* string, unsigned char* padrao) {
-   size_t t = strlen(padrao);
-   size_t u = strlen(string);
+static OutcomeStrings reparte_string_by_char(char* string, const char sep) 
+{
+   /* Separa em string à partir de qualquer caractére dado, seja ele 
+    * 'imprimível' ou não.
+    */
+   const char SEPARADOR = sep;
+   char* ptr_string = string;
    size_t contagem = 0;
-   size_t capacidade = 13;
-   size_t indices[capacidade]; 
+   size_t posicoes[strlen(string)];
 
-   /* marcando os índices de acordo com a iteração. */
-   for (size_t i = 0; i < u; i++) {
-      slice_t e = {string, i, i + t};
+   // contando o total de palavras na string.
+   while ((ptr_string = strchr (ptr_string, SEPARADOR)) != NULL) {
+      size_t T = strlen(string);
+      size_t t = strlen(ptr_string);
+      size_t atual_posicao = T - t + 1;
 
-      if (compara_slice_e_str(e, string)) {
-         indices[contagem] = i;
-         #ifdef _DEBUG
-         printf("%lu\n", i);
-         #endif
-         contagem++;
-      }
+      // contando o espaço em branco, e inserido a posição dele na string.
+      posicoes[contagem++] = atual_posicao;
+      // indo a próximo caractére...
+      ptr_string++;
    }
-   return (indices_t){ indices, contagem + 1 };
+   #ifdef _PALAVRAS
+   printf ("quantidade: %lu\nposições na string:", contagem);
+   visualiza_array(posicoes, contagem);
+   #endif
+
+   // repartindo ela em palavras e axexando na lista.
+   size_t size_ptr_char =  sizeof (char*);
+   // o total de espaços branco mais um é o total de 'palavras'.
+   size_t total = contagem + 1;
+   char** lista = malloc (total * size_ptr_char);
+   bool existe_palavras = contagem >= 1;
+
+   // cuidando das pontas ...
+   if (existe_palavras) {
+      // primeira palavra:
+      // descotando a posição que começa a próxima palavra.
+      char* first_word = copia_trecho (string, 0, posicoes[0] - 1);
+      lista[0] = first_word;
+      // última palavra:
+      char* last_word = copia_trecho (
+         string, 
+         posicoes[contagem - 1], 
+         strlen(string)
+      );
+      lista[total - 1] = last_word;
+
+      #ifdef _PALAVRAS
+      printf ("\tprimeira palavra: '%s'\n", first_word);
+      printf ("\túltima palavra: '%s'\n", last_word);
+      #endif
+   }
+
+   for (size_t i = 1; i < contagem; i++) {
+      size_t inicio = posicoes[i - 1];
+      // descotando a posição que começa a próxima palavra.
+      size_t final = posicoes[i] - 1;
+      char* word = copia_trecho (string, inicio, final);
+      lista[i] = word;
+
+      #ifdef _PALAVRAS
+      printf ("\tpalavra: '%s'\n", word);
+      #endif
+   }
+   return (OutcomePalavras) {lista, total};
 }
 
-static slice_t* split(unsigned char* string, unsigned char* padrao) {
-   indices_t array = indices_buscado(string, padrao);
+OutcomePalavras palavras(char* string) {
+   /* Pega todos trechos entre espaços em branco, sejam eles palavras 
+    * legítimas ou não. */
+   const char ESPACO_SEPARADOR = ' ';
+   return reparte_string_by_char (string, ESPACO_SEPARADOR);
+}
 
-   if (array.total == 0)
-      return NULL;
+OutcomeStrings split_linhas(char* conteudo) {
+   /* Dado uma grande string, está separada com quebras-de-linhas, aqui
+    * será retornado uma cópia de todas linhas do conteúdo(sendo este 
+    * provavalmente vindo de algum arquivo). */
+   const char ESPACO_SEPARADOR = '\n';
+   return reparte_string_by_char (string, ESPACO_SEPARADOR);
+}
 
-   for (size_t k = 0; k < array.total - 1; k++) {
-      slice_t e = { string, array.indices[k], array.indices[k + 1]};
-      imprime_slice(e);
+char* concatena_strings(uint8_t quantia, ...) {
+   /* Cocatena uma quantia limitada de strings, dado a quantia delas. 
+    * Nota: Como tal argumentos para função só podem ser manualmente o 
+    *    tipo de inteiros que ele aceita é um de 8 bits, ou seja, só cabem
+    *    apenas 256 argumentos tal função. Bastante, porém limitada, mas 
+    *    mesmo com isso é um baita de quebra-galho para o tipo existente. */
+   va_list argumentos;
+   va_start (argumentos, quantia);
+   size_t comprimento = 0;
+   char* referencias[UINT8_MAX];
+
+   for (size_t i = 1; i <= quantia; i++) {
+      char* strptr = va_arg (argumentos, char*);   
+      comprimento += strlen (strptr);
+      // guardando a string para futura concatenação.
+      referencias[i - 1] = strptr;
+
+      #ifdef _CONCATENA_STRINGS
+      printf ("string extraída: '%s'\n", strptr);
+      #endif
    }
+   va_end (argumentos);
+   // contando caractére nulo no final.
+   comprimento++;
 
-   return NULL;
+   #ifdef _CONCATENA_STRINGS
+   printf ("total de strings dadas: %u\n", quantia);
+   printf ("comprimento calculado: %lu\n", comprimento);
+   #endif
+
+   char* grande_string = malloc (quantia * sizeof (char) * comprimento);
+   size_t cursor = 0;
+   // criando string concatenada...
+   for (size_t i = 1; i <= quantia; i++) {
+      char* atualstr = referencias[i - 1];
+      while (*atualstr != '\0') {
+         grande_string[cursor++] = *atualstr;
+         atualstr++; 
+      }
+   }
+   grande_string[comprimento - 1] = '\0';
+   return grande_string;
 }
 
 #ifdef _UT_STRING
-
-char silly_poem[] = (
-   "roses are red; violets are blue; i see you big pretty"
-   " eyes... as a huge cloud comes me soon"
-);
-
-unsigned char frase_i[] = "hoje será um bom dia!";
-
+// bibliotecas essenciais:
 #include <stdint.h>
-
-void iterando_slice_string() {
-   slice_t s = { silly_poem, 6, 28};
-
-   imprime_slice(s);
-   for (size_t p = s.inicio; p <= s.final; p++)
-      printf("|%c| ", s.string[p]);
-   puts("\b\n");
-
-   slice_t si = { frase_i, 6, 15};
-   for (size_t p = si.inicio; p <= si.final; p++)
-      printf("|%c|(%x) ", si.string[p], (uint8_t)si.string[p]);
-   puts("\b\n");
-
-}
-
-void visualizando_slices() {
-   puts(frase_i);
-   slice_t s1 = { frase_i, 5, 8};
-   imprime_slice(s1);
-   slice_t s0 = { frase_i, 5, 9};
-   imprime_slice(s0);
-
-   slice_t s2 = { frase_i, 13, 15};
-   imprime_slice(s2);
-
-   slice_t s3 = { frase_i, 10, 11 };
-   imprime_slice(s3);
-
-   slice_t s4 = { frase_i, 0, 3};
-   imprime_slice(s4);
-}
-
 #include "teste.h"
+#include "dados_testes.h"
 #include <stdlib.h>
 
-int main(int qtd, char* args[], char* vars[]) {
+void testes_basico_da_reparticao_em_palavras(void) {
+   for (size_t i = 1; i <= 5; i++) {
+      char* phrase = frases_i[i - 1];
+      printf ("\nsobre: '%s'\n", phrase);
+      palavras(phrase);
+   }
+}
 
+void visualizacao_da_reparticao_em_palavras(void) {
+   OutcomePalavras result = palavras (frase_ii);
+
+   puts ("listando o resultado: ...");
+   for (size_t q = 1; q <= result.total; q++)
+      printf ("\t==>  '%s'\n", result.lista[q - 1]);
+}
+
+void experimento_concatenacao_de_multiplas_strings(void) {
+   char* result = concatena_strings (
+      4, frutas[3], frutas[5], 
+         frutas[7], frutas[8]
+   );
+   printf ("a concatenação: '%s'\n", result);
+}
+
+int main(int qtd, char* args[], char* vars[]) {
    executa_testes(
-      2, visualizando_slices, true,
-      iterando_slice_string, true
+      3, testes_basico_da_reparticao_em_palavras, true,
+         visualizacao_da_reparticao_em_palavras, true,
+         experimento_concatenacao_de_multiplas_strings, true
    );
 
    return EXIT_SUCCESS;
