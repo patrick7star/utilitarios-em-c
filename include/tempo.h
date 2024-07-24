b248aa1ad01e136aebbaf22a8094bc0ced42dc02
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-07-24 01:27:57 -0300

    testando algo...

--- include/tempo.h
+++ include/tempo.h
@@ -7,24 +7,24 @@
 #define TEMPO_H
  typedef enum tipo_de_tempo { 
    // Tempo normal:
-   Segundo, 
-   Minuto, 
-   Hora, 
+   Hora, Minuto, Segundo, 
    // Abaixo de um segundo:
-   Miliseg, 
-   Microseg, 
-   Nanoseg 
+   Miliseg, Microseg, Nanoseg 
 
  } TempoTipo, TEMPO_TIPO;
- 
+
  /* Apelidos da estruturas -- por mais que os métodos só exaltem apenas um,
   * os demais também são válidos; todos seus métodos e funções relacionadas
   * com a estrutura.
   *   Todos respectivos apelidos para o tipo de dado também são postos
   * abaixo. Os dois primeiros "incorporam" o ponteiro para você não se 
   * preocupar com isso, já o último é preciso específicar-lô. */
- typedef struct cronometro *CRONOMETRO, *Cronometro, cronometro_t;
- typedef struct contador_regressivo *Temporizador, TIMER, temporizador_t;
+ typedef struct cronometro 
+   CRONOMETRO, *Cronometro, 
+   cronometro_t;
+ typedef struct contador_regressivo 
+   *Temporizador, *Timer, 
+   TIMER, temporizador_t;
 
  /* executa uma pausa no programa dado a grandeza do tempo, e o total de 
   * unidades daquela grandeza. */
@@ -33,19 +33,20 @@
  /* Operações de ambas estruturas de dados acima. */
 
  // construtor e destruturo do 'cronômetro'.
- extern CRONOMETRO cria_cronometro(); 
- extern void destroi_cronometro(CRONOMETRO c, bool info); 
+ extern Cronometro cria_cronometro(); 
+ void destroi_cronometro(Cronometro); 
 
  // operações sobre o cronômetro:
- extern double marca(CRONOMETRO c); 
- extern double variacao(CRONOMETRO c); 
+ extern double marca(Cronometro c); 
+ /* Computa a variação deste o último registro marcado, se houve algum, se
+  * não, então computa o tempo decorrido desde o ínicio da contagem.*/
+ extern double variacao(Cronometro c); 
 
  // visualização de registros feitos no 'cronômetro'.
- extern void visualiza_marcos(CRONOMETRO c); 
- /* retorna a formatação string do cronômetro, sendo o nome do 
-  * tipo de dado, o tempo decorrido até então, e o total de 
-  * marcações até agora. */
- extern char* cronometro_to_str(Cronometro c); 
+ extern void visualiza_marcos(Cronometro c); 
+ /* Retorna a formatação string do cronômetro, sendo o nome do tipo de 
+  * dado, o tempo decorrido até então, e o total de marcações até agora. */
+ char* cronometro_to_str(Cronometro); 
 
 
  // construtor e destrutor do 'temporizador'.
@@ -60,7 +61,7 @@
  extern float percentual (Temporizador);
 
  // ainda em fase de testes:
- extern char* temporizador_to_str (Temporizador);
- extern void recomecar (Temporizador);
+ char* timer_to_str (Temporizador);
+ void recomecar (Temporizador);
 
 #endif //TEMPO_H
