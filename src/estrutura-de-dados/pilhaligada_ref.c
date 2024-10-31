c78482651ce411d1db5db4cb0f2d997cf95c57c5
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-10-31 06:41:24 -0300

    inversão e clonagem da pilha adicionados
    Será a primeira a ter tipo de funçõa habilitadas.

--- src/estrutura-de-dados/pilhaligada_ref.c
+++ src/estrutura-de-dados/pilhaligada_ref.c
@@ -16,7 +16,7 @@
 /* === === === === === === === === === === === === === === === === === ==
  *                      Nódulo da lista-ligada
  *
- * Abstração da lista-ligada que forma a pilha, e porta a referência ao 
+ * Abstração da lista-ligada que forma a pilha, e porta a referência ao
  * dado original.
  * === === === === === === === === === === === === === === === === === ==*/
 typedef struct nodulo {
@@ -33,7 +33,7 @@ typedef struct nodulo {
 
 Node cria_nodulo(generico_t dado) {
    Node instancia = malloc (NODULO_SZ);
-   
+  
    if (instancia != NULL) {
       instancia->dado = dado;
       instancia->seta = NULL;
@@ -66,8 +66,10 @@ bool destroi_nodulo_e_dado(Node x, Drop descontrutor) {
    x = NULL;
    return true;
 }
-// === === === === === === === === === === === === === === === === === ===
-
+/* === === === === === === === === === === === === === === === === === ==
+ *                      Declaração e Implementação
+ *                            da Estrutura
+ * === === === === === === === === === === === === === === === === === ==*/
 struct pilha_ligada_abstracao {
    // quantificando quantos elementos ela realmente tem.
    size_t quantidade;
@@ -103,10 +105,10 @@ bool coloca_pl(PilhaLigada s, void* dado) {
    return true;
 }
 
-size_t comprimento_pl(PilhaLigada s) 
+size_t comprimento_pl(PilhaLigada s)
    { return s->quantidade; }
 
-bool vazia_pl(PilhaLigada s) 
+bool vazia_pl(PilhaLigada s)
    { return s->quantidade == 0; }
 
 void visualiza_pl(PilhaLigada s) {
@@ -133,7 +135,7 @@ generico_t retira_pl(PilhaLigada s) {
    destroi_nodulo (item_removido);
    // descontabilizando o item removido.
    s->quantidade -= 1;
-   
+  
    return dado_extraido;
 }
 
@@ -165,14 +167,14 @@ bool destroi_pl (PilhaLigada s) {
 
    #ifdef _DESTROI_PL
    size_t contagem = 0;
-   #endif 
+   #endif
 
    while (!vazia_pl (s)) {
       retira_pl (s);
 
       #ifdef _DESTROI_PL
       contagem++;
-      #endif 
+      #endif
    }
 
    #ifdef _DESTROI_PL
@@ -186,7 +188,7 @@ bool destroi_pl (PilhaLigada s) {
 
 bool destroi_interno_pl (PilhaLigada s, Drop del) {
 /* Mesmo fim que o método de desalocação acima, porém aqui também libera
- * o dado passado junto, ou seja, provavelmente tal pilha contém um 
+ * o dado passado junto, ou seja, provavelmente tal pilha contém um
  * monte de dado na heap, sendo tal no máximo uma array de algum tipo,
  * digo de fácil desalocação. */
 
@@ -208,10 +210,11 @@ bool destroi_interno_pl (PilhaLigada s, Drop del) {
       assert (dado == NULL);
       size_t quantia =  comprimento_pl(s);
       bool chegou = quantia < parada;
+
       if (chegou) {
          printf (
             "%03luº. memória cortada à metade!\n"
-            "qtd. de itens=%8lu\n", 
+            "qtd. de itens=%8lu\n",
             ++cortes, quantia
          );
          parada = quantia / 2;
@@ -238,10 +241,10 @@ size_t tamanho_pl(PilhaLigada s, size_t size_dt) {
  *
  * Inicialmente, será apenas uma cópia do iterador da estrutura conjunto.
  * Depois começam os retoques para atual estrutura(uma lista-ligada). Aqui,
- * nem todos campos são importantes, então serão removidos, já que são 
+ * nem todos campos são importantes, então serão removidos, já que são
  * relevantes apenas a estrutura hashtable(tipo o campo índice). O algoritmo
  * iteração, o mais importante, é mesmo mais simples que o para a estrutura
- * de 'tabela de dispersão', no mais, pilha-ligada é apenas uma simples 
+ * de 'tabela de dispersão', no mais, pilha-ligada é apenas uma simples
  * lista-ligada que adiciona e remove em uma ponta.
  * === === === === === === === === === === === === === === === === === ==*/
 struct iterador_da_pilha_pl {
@@ -274,13 +277,13 @@ IterPL cria_iter_pl(PilhaLigada a) {
    return self;
 }
 
-bool consumido_iter_pl(IterPL iter) 
+bool consumido_iter_pl(IterPL iter)
    { return iter->contagem == iter->inicial; }
 
 size_t contagem_iter_set (IterPL iter) {
    // Verificando se o iterador é válido.
    bool iterador_e_valido = {
-      iter != NULL 
+      iter != NULL
       && !consumido_iter_pl(iter)
    };
 
@@ -302,7 +305,7 @@ IterOutputPL next_pl (IterPL iter) {
    iter->cursor = iter->cursor->seta;
    // Contabilizando iterações realizadas.
    iter->contagem += 1;
-   
+  
    return (IterOutputPL){ .item=dado_no_nodulo };
 }
 
@@ -321,7 +324,7 @@ IterPL clona_iter_pl(IterPL iter) {
 
 void destroi_iter_pl(IterPL a) {
    // Não segura mais o container da estrutura.
-   a->instancia = NULL; 
+   a->instancia = NULL;
    // Livra-se também da referência do atual nódulo iterador nela.
    a->cursor =NULL;
    // libera tal instância.
@@ -361,7 +364,7 @@ char* stack_to_str_pl(PilhaLigada s, ToString f) {
 
       comprimento += strlen(string_fmt) + 2;
       free(string_fmt);
-   } while (!consumido_iter_pl(i)); 
+   } while (!consumido_iter_pl(i));
 
    // Ajustando o comprimento para o cabeçalho, e os delimitadores:
    comprimento += 50;
@@ -384,7 +387,7 @@ char* stack_to_str_pl(PilhaLigada s, ToString f) {
       strcat(resultado_fmt, ", ");
       // Libera string gerada momentaneamente.
       free(dado_fmt);
-   } 
+   }
 
    // Finalizando a lista em forma de string, então, retornando...
    if (vazia_pl(s) || t == 1)
@@ -410,15 +413,15 @@ void imprime_pl(PilhaLigada s, ToString f) {
 void extende_pl(PilhaLigada s, PilhaLigada p) {
 /* Sendo a pilha do primeiro argumento a "principal", ele pega a segunda,
  * extrai todos seus itens, então adiciona(empilha) na primeira. A abstração
- * aqui é: você tem duas pilhas, não importa o tamanho, você pega uma pilha, * digo a sua base, e coloca em cima de outra, o topo desta segunda deve 
- * ser agora o topo da pilha extendida, a outra pilha se torna vázia ou 
- * inexistente. Apesar de retirar todos seus itens para extensão, não 
+ * aqui é: você tem duas pilhas, não importa o tamanho, você pega uma pilha, * digo a sua base, e coloca em cima de outra, o topo desta segunda deve
+ * ser agora o topo da pilha extendida, a outra pilha se torna vázia ou
+ * inexistente. Apesar de retirar todos seus itens para extensão, não
  * destroi(desaloca) ela.
  */
    PilhaLigada aux = cria_pl();
 
-   /* Colocando numa pilha auxiliar, pois realocar está pilha auxiliar na 
-    * principal fará que a ordem não fique invertida, portanto o topo 
+   /* Colocando numa pilha auxiliar, pois realocar está pilha auxiliar na
+    * principal fará que a ordem não fique invertida, portanto o topo
     * da secundária será também o topo da principal, formando assim uma
     * extensão de pilhas. */
    while (!vazia_pl(p)) {
@@ -434,7 +437,7 @@ void extende_pl(PilhaLigada s, PilhaLigada p) {
 }
 
 generico_t* pilha_to_array_pl(PilhaLigada S) {
-/* Faz uma cópia da pilha, e a transforma numa array, seguindo é claro a 
+/* Faz uma cópia da pilha, e a transforma numa array, seguindo é claro a
  * propriedade de uma(LIFO). */
    bool nenhum_item_na_pilha = vazia_pl(S);
 
@@ -454,8 +457,69 @@ generico_t* pilha_to_array_pl(PilhaLigada S) {
    return the_array;
 }
 
+void inverte_pl(PilhaLigada s)
+{
+/* Não será trabalhado na inversão de uma fila, usando as propriedades do
+ * tipo, porque isso aqui é um modo de aproximar o algoritmo de inversão
+ * de uma lista-encadeada.
+ */
+   Node anterior = NULL;
+   Node atual    = s->topo;
+   Node proximo  = NULL;
+
+   // Itera a lista 'o total de itens' vezes.
+   // for (size_t i = 1; i <= N; i++)
+   while (atual != NULL)
+   {
+      proximo     = atual->seta;
+      atual->seta = anterior;
+      anterior    = atual;
+      atual       = proximo;
+   }
+   // O topo pega o anterior ao fim da lista.
+   s->topo = anterior;
+}
+
+PilhaLigada clona_pl(PilhaLigada s, Clone f)
+{
+/* Tendo a função que clona o tipo de dado que a pilha-ligada retém. Vamos
+ * duplicar está instância, nos moldes que está seu estado. O algoritmo aqui
+ * segue a abstração de pilha, ou seja, removemos cada item, clonamos, e
+ * colocamos o original de volta, e o clone na pilha de cópia. Depois
+ * revertemos ambas elas pois a iteração de remoção fez ficarem reviradas.
+ * Por fim, retorna o clone da original. */
+
+   // Pilha que ficará os itens clonados, e uma pilha temporaria.
+   PilhaLigada e = cria_pl(), a = cria_pl();
+   size_t qtd = comprimento_pl(s);
+
+   while(qtd-- > 0) {
+      generico_t remocao = retira_pl(s);
+      generico_t copia = f(remocao);
+
+      // Inserindo cópia de item, e original removido em temporária.
+      assert(coloca_pl(e, copia));
+      assert(coloca_pl(a, remocao));
+   }
+
+   /* Decarrega temporária novamente, porém agora na inicial. Tal iteração
+    * para reposição de volta já dispensa o processo de inverter a original
+    * novamente. */
+   do {
+      generico_t remocao = retira_pl(a);
+      assert(coloca_pl(s, remocao));
+
+   } while(!vazia_pl(a));
+
+   /* Inverte a copia, porque a iteração de copia e inserção faz ela ficar
+    * revirada. Libera a pilha temporária que está vázia. E por último,
+    * retorna a clonagem da pilha. */
+   inverte_pl(e);
+   destroi_pl(a);
+   return e;
+}
 /* === === === === === === === === === === === === === === === === === ==
- *                Tradução de funções e métodos acimas 
+ *                Tradução de funções e métodos acimas
  *
  * Nem todas as funções tem uma tradução, inicialmente pelo menos, já que
  * futuramente serão adicionadas. Todas elas serão apenas um embrulha com
@@ -464,7 +528,7 @@ generico_t* pilha_to_array_pl(PilhaLigada S) {
  * métodos e tipos de dados não estão aqui, porque eles já estão de certo
  * modo em inglês, já outros, tenho que ainda decidir um nome.
  * === === === === === === === === === === === === === === === === === ==*/
-PilhaLigada new_pl (void) 
+PilhaLigada new_pl (void)
    { return cria_pl(); }
 
 bool delete_pl (PilhaLigada s)
@@ -485,14 +549,14 @@ bool empty_pl (PilhaLigada s)
 void* top_pl (PilhaLigada s)
    { return topo_pl(s); }
 
-void print_pl (PilhaLigada s, ToString f) 
+void print_pl (PilhaLigada s, ToString f)
    { imprime_pl(s, f);  }
 
-generico_t* stack_to_array_pl(PilhaLigada S) 
+generico_t* stack_to_array_pl(PilhaLigada S)
    { return pilha_to_array_pl(S); }
 
 /* === === === === === === === === === === === === === === === === === ==
- *                      Testes Unitários 
+ *                      Testes Unitários
  * === === === === === === === === === === === === === === === === === ==*/
 #ifdef _UT_PILHA_LIGADA
 #include <locale.h>
@@ -552,14 +616,14 @@ size_t computa_tamanho_da_pilha (PilhaLigada s) {
    Node atual = s->topo;
 
    do {
-      // T += sizeof (nodulo_t); 
+      // T += sizeof (nodulo_t);
       T += NODULO_SZ;
       atual = atual->seta;
    } while (atual != NULL);
    return T;
 }
 
-size_t unidades_por_megabytes (uint16_t nMB, size_t size) 
+size_t unidades_por_megabytes (uint16_t nMB, size_t size)
    { return nMB * pow (2, 20) / size; }
 
 bool deleta_u16(generico_t dt) { free(dt); return true; }
@@ -577,7 +641,7 @@ void verificando_vazamento_de_memoria (void) {
 
    for (size_t i = 1; i <= total_de_unidades; i++) {
       X = calloc (1, sizeof(int));
-      *X = rand() % (30 + 1); 
+      *X = rand() % (30 + 1);
 
       coloca_pl (stack, X);
       atualiza_e_visualiza_bp (&barra, i);
@@ -616,7 +680,7 @@ void estruturas_tamanhos (void) {
    );
    assert (pl_bytes == instancia_bytes);
    printf (
-      "instância total é %lu bytes.\n", 
+      "instância total é %lu bytes.\n",
       computa_tamanho_da_pilha(stack)
    );
    destroi_pl (stack);
@@ -632,7 +696,7 @@ void verificando_vazamento_de_memoria_i (void) {
    // um bocado de zeros:
    int* mesmo_dado = calloc (Q, sizeof (int));
    puts ("array de 150MB:");
-   for (size_t k = 1; k <= Q; k++) 
+   for (size_t k = 1; k <= Q; k++)
       mesmo_dado[k - 1] = 8;
 
    // adiciona na array valores randômicos de 0 até 30.
@@ -698,17 +762,82 @@ void amostra_simples_de_todos_seus_metodos(void) {
    destroi_pl(stack);
 }
 
+void processo_de_inversao_da_lista_ligada(void) {
+   PilhaLigada stack = cria_pl();
+
+   for (size_t k = 0; k < FRUTAS; k++) {
+      char* e = (char*)frutas[k];
+      assert (coloca_pl(stack, e));
+   }
+
+   print_pl(stack, conststr_to_str);
+
+   inverte_pl(stack);
+   print_pl(stack, conststr_to_str);
+
+   printf("\nRemovendo na fila-ligada invertida...");
+   do {
+      generico_t a = pop_pl(stack);
+      char* b = (char*)a;
+      printf("\t\b\b\b- '%s'\n", b);
+
+   } while(!empty_pl(stack));
+
+   puts("\nTeste de inversão com um e dois itens apenas:");
+   assert(coloca_pl(stack, "nada"));
+   assert(coloca_pl(stack, "nenhuma"));
+   print_pl(stack, conststr_to_str);
+   inverte_pl(stack);
+   print_pl(stack, conststr_to_str);
+}
+
+generico_t clona_constchar(generico_t a)
+{
+   if (a == NULL) {
+      perror("Valor inválido");
+      abort();
+   }
+
+   char* obj = (char*)a;
+   int t = strlen(obj);
+   int sz = (t + 1) * sizeof(char);
+   char* clone = malloc(sz);
+
+   memcpy(clone, obj, sz);
+   return clone;
+}
+
+bool free_str(generico_t b) { free(b); return true; }
+
+void metodo_de_clonagem_da_pilha(void) {
+   PilhaLigada stack = cria_pl();
+   const int N = OBJETOS / 2;
+
+   for (size_t k = 0; k < N; k++) {
+      char* e = (char*)objetos[k];
+      assert (coloca_pl(stack, e));
+   }
+
+   print_pl(stack, conststr_to_str);
+
+   PilhaLigada S = clona_pl(stack, clona_constchar);
+   print_pl(S, conststr_to_str);
+   destroi_interno_pl(S, free_str);
+}
+
 int main(int total, char* args[], char* vars[]) {
    // permitindo impressões de caractéres 'latin1'.
    setlocale(LC_CTYPE, "pt_BR.UTF-8");
 
    executa_testes(
-      6, pilha_com_i32s, true,
-         pilha_de_strings, true,
-         amostra_simples_de_todos_seus_metodos, true,
+      8, pilha_com_i32s, false,
+         pilha_de_strings, false,
+         amostra_simples_de_todos_seus_metodos, false,
          // Desativada pois consome bastante CPU e memória:
          verificando_vazamento_de_memoria, false,
          verificando_vazamento_de_memoria_i, false,
+         processo_de_inversao_da_lista_ligada, true,
+         metodo_de_clonagem_da_pilha, true,
          // Verificando algumas coisas da linguagem:
          estruturas_tamanhos, false
    );
