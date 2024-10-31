ece9e8d86eb250a70732affe9d41fad59c050a13
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-10-31 06:38:42 -0300

    novos métodos adicionados.

--- include/definicoes.h
+++ include/definicoes.h
@@ -1,7 +1,7 @@
 /* Muitos das implementações das outras estruturas definidas neste diretório
  * usam muito estas definições. Aqui reescrevo elas, e nas demais, serão
  * simplesmente importadas daqui. Melhora as definições do demais, deixando
- * menos coisas no 'cabeçalho', e cada uma não fica reimplementando as 
+ * menos coisas no 'cabeçalho', e cada uma não fica reimplementando as
  * mesmas definições, mesmo que os impactos por causa disso até agora sejam
  * nulos.
  */
@@ -13,9 +13,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 
- /* O ponteiro do tipo void é algo parecido, mais longe de ser igual -- 
-  * vamos dizer que permite realizar o mesmo -- função de genéricos em 
-  * várias linguagens. 
+ /* O ponteiro do tipo void é algo parecido, mais longe de ser igual --
+  * vamos dizer que permite realizar o mesmo -- função de genéricos em
+  * várias linguagens.
   */
  typedef void* generico_t;
 
@@ -28,13 +28,16 @@
  typedef bool (*Drop)(generico_t);
  // Propriedade de transformar o dado genérico numa string.
  typedef char* (*ToString)(generico_t);
+ // Template de função da clonagem(aplica uma 'deepcopy') de um dado.
+ typedef generico_t (*Clone)(generico_t);
 
- /*   A notação acima é bem Javeira e Rustcean, aqui vem como algo mais 
+ /*  A notação acima é bem Javeira e Rustcean, aqui vem como algo mais
   * Pythonico. */
- typedef Drop __del__;
+ typedef Drop     __del__;
  typedef ToString __str__;
- typedef Hash __hash__;
- typedef Eq __eq__;
+ typedef Hash     __hash__;
+ typedef Eq       __eq__;
+ typedef Clone    __clone__;
  /* Comparações de menor, maior, e menor/maior e igual. */
  typedef bool (*__lt__)(generico_t, generico_t);
  typedef bool (*__le__)(generico_t, generico_t);
--- include/pilhaligada_ref.h
+++ include/pilhaligada_ref.h
@@ -4,22 +4,24 @@
 #include "definicoes.h"
 
  // Todos nomes que eles podem assumir.
- typedef struct pilha_ligada_abstracao 
-   *PilhaLigada, PL, pilha_ligada_t, 
+ typedef struct pilha_ligada_abstracao
+   *PilhaLigada, PL, pilha_ligada_t,
    PILHA_LIGADA, *LinkedStack;
 
  // Funções que criam e destró a instância do tipo de dado:
  PilhaLigada cria_pl (void);
- bool destroi_pl (PilhaLigada); 
- bool destroi_interno_pl (PilhaLigada, Drop); 
+ bool destroi_pl (PilhaLigada);
+ bool destroi_interno_pl (PilhaLigada, Drop);
 
  // Operações alteram a pilha:
- bool coloca_pl (PilhaLigada, generico_t); 
- generico_t retira_pl (PilhaLigada); 
- /* Pega a base da segunda pilha(segundo parâmetro), então coloca ela na 
+ bool coloca_pl (PilhaLigada, generico_t);
+ generico_t retira_pl (PilhaLigada);
+ /* Pega a base da segunda pilha(segundo parâmetro), então coloca ela na
   * primeira(o primeiro parâmetro da função). A primeira pilha fica assim
   * extendida, a segunda vázia. */
  void extende_pl (PilhaLigada, PilhaLigada);
+ // Inverte a pilha, topo vira base, base vira topo.
+ void inverte_pl (PilhaLigada);
 
  /* Operações que verificam informações sobre as estruturas: total de itens,
   * valor no topo da pilha e etc. */
@@ -27,16 +29,16 @@
  size_t tamanho_pl (PilhaLigada, size_t);
  bool vazia_pl (PilhaLigada);
  void* topo_pl (PilhaLigada);
- 
- /* Operação que permite visualizar a pilha, assim como formata-la em 
+
+ /* Operação que permite visualizar a pilha, assim como formata-la em
   * uma string. */
- void imprime_pl (PilhaLigada, ToString); 
+ void imprime_pl (PilhaLigada, ToString);
  char* stack_to_str_pl (PilhaLigada, ToString);
 
  /* Cria uma array, com a mesma quantidade de itens na pilha, com todos
   * itens empilhados do topo à base, que na array fica respectivamente
   * da esquerda à direita. */
- generico_t* pilha_to_array_pl (PilhaLigada); 
+ generico_t* pilha_to_array_pl (PilhaLigada);
 
 /* === === === === === === === === === === === === === === === === === ==
  *                      Iteradores e seus
@@ -54,12 +56,12 @@
  IterPL clona_iter_pl(IteradorPL);
 
  // Acesso a informação ou muda sua estrutura:
- bool consumido_iter_pl (IteradorPL); 
+ bool consumido_iter_pl (IteradorPL);
  size_t contagem_iter_set (IteradorPL);
  IterOutputPL next_pl (IteradorPL);
 
 /* === === === === === === === === === === === === === === === === === ==
- *                      Renomeação de vários 
+ *                      Renomeação de vários
  *                métodos e funções para os termos
  *                  mais conhecidos em inglês
  *
@@ -67,17 +69,17 @@
  * para facilitação na hora de achar os correspodentes.
  * === === === === === === === === === === === === === === === === === ==*/
  PilhaLigada new_pl (void);
- bool delete_pl (PilhaLigada); 
+ bool delete_pl (PilhaLigada);
 
- bool push_pl(PilhaLigada, generico_t); 
- generico_t pop_pl(PilhaLigada); 
+ bool push_pl(PilhaLigada, generico_t);
+ generico_t pop_pl(PilhaLigada);
 
  size_t len_pl (PilhaLigada);
  bool empty_pl (PilhaLigada);
  void* top_pl (PilhaLigada);
 
- void print_pl (PilhaLigada, ToString); 
+ void print_pl (PilhaLigada, ToString);
 
- generico_t* stack_to_array_pl (PilhaLigada); 
+ generico_t* stack_to_array_pl (PilhaLigada);
 
 #endif
