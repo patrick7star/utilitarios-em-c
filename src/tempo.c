690086a17addf877b6888499d1698c3e001726c5
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2025-01-06 01:54:30 -0300

    porte para Windows de tal módulo

--- src/tempo.c
+++ src/tempo.c
@@ -10,21 +10,22 @@
 #include <math.h>
 #include <inttypes.h>
 #include <stdio.h>
-#include <threads.h>
 #include "tempo.h"
 
 
 #include "tempo/calculo_potencia.c"
 
-/* pausa a thread principal é extremamente complicado de se fazer
+/* Pausa a thread principal é extremamente complicado de se fazer
  * com a função da biblioteca padrão, então aqui vou cria um 
  * menos complicadinho. 
  */
-void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd) {
-   // só é permitido valores menores que mil, pois qualquer coisa 
-   // acima quebra os limite do 'Tipo de Tempo'.
+void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd) 
+{
+	#ifdef __linux__
+   /* Só é permitido valores menores que mil, pois qualquer coisa 
+    * acima quebra os limite do 'Tipo de Tempo'. */
    if (qtd > 1000)
-      { perror("não pode passar para o tipo."); }
+      { perror("não pode passar para o tipo."); abort(); }
 
    struct timespec periodo;
    switch(tipo) {
@@ -58,6 +59,10 @@ void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd) {
 
    // aplicando o tempo depois de configurado.
    thrd_sleep(&periodo, NULL);
+	#elif defined(_WIN32)
+	perror("Não implementado para Windows ainda.");
+	abort();
+	#endif
 }
 
 /* Importando o restante do código para cá. Funções, estruturas, 
@@ -70,7 +75,13 @@ void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd) {
  * podem ser mudadas.
  */
 #include "tempo/cronometro.c"   // primeiro
+#ifdef __linux__
+#include <threads.h>
 #include "tempo/temporizador.c" // segundo.
+#elif defined(_WIN32)
+#include "tempo/temporizador-win.c" // segundo.
+#endif
+
 
 /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
  *
@@ -79,8 +90,13 @@ void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd) {
  * --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---*/
 #if defined(_UT_TEMPO)
 #include <assert.h>
+#ifdef __linux__
 #include <unistd.h>
+#elif defined(_WIN32)
+#include <locale.h>
+#endif
 
+#ifdef __linux__
 void alocacao_e_desalocacao() {
    Cronometro c = cria_cronometro();
    // pausa.
@@ -258,9 +274,180 @@ void teste_desconhecido_ii(void) {
    size_t decorrido_nano = fim.tv_nsec - inicio.tv_nsec;
    printf("%ldseg %ldns\n", decorrido_seg, decorrido_nano);
 }
+#elif defined(_WIN32)
+#include <synchapi.h>
+
+#define CABECALHO_DA_FUNCAO(void) ({\
+	printf( \
+		"\n\n[linha: %d] Função em execução agora: '%s'\n", \
+		__LINE__, __FUNCTION__ \
+	);\
+})
+
+void alocacao_e_desalocacao() {
+	CABECALHO_DA_FUNCAO();
+   Cronometro c = cria_cronometro();
+   // pausa.
+	Sleep(2700);
+   destroi_cronometro(c);
+
+   Cronometro c1 = cria_cronometro();
+	Sleep(26);
+   destroi_cronometro(c1);
+}
+
+void visualizacao_dos_marcos() {
+	CABECALHO_DA_FUNCAO();
+
+   Cronometro clock = cria_cronometro();
+	Sleep(1000);
+
+   for (int i = 0; i < 95; i++) { 
+      marca(clock);
+      if (i % 6 != 0)
+         printf("registrado\t");
+      else
+         puts("");
+		Sleep(rand() % 150 + 50);
+   }
+   // invocando a função que faz a listagem.
+   puts("\nregistros feitos:");
+   visualiza_marcos(clock);
+}
+
+void stringficacao_do_temporizador() {
+	CABECALHO_DA_FUNCAO();
+
+   Temporizador timer = cria_temporizador(Miliseg, 20);
+   assert(timer->reutilizacoes == 0);
+
+	Sleep(500);
+   puts(timer_to_str(timer));
+
+	Sleep(6);
+   puts(timer_to_str(timer));
+
+   recomecar(timer);
+   assert(timer->reutilizacoes == 1);
+
+	Sleep(3);
+   puts(timer_to_str(timer));
+
+	Sleep(12);
+   puts(timer_to_str(timer));
+
+	Sleep(8);
+   puts(timer_to_str(timer));
+
+   destroi_temporizador(timer);
+
+   Temporizador timer_seg = cria_temporizador(Segundo, 8);
+
+	Sleep(3000);
+   puts(timer_to_str(timer_seg));
+
+	Sleep(4000);
+   puts(timer_to_str(timer_seg));
+
+	Sleep(2000);
+   puts(timer_to_str(timer_seg));
+
+   destroi_temporizador(timer_seg);
+}
+
+void dois_tais_ate_o_esgotamento(void) {
+	CABECALHO_DA_FUNCAO();
+
+   Temporizador a = cria_temporizador(Segundo, 8);
+   Temporizador b = cria_temporizador(Segundo, 13);
+
+   while (!(esgotado(a) && esgotado(b))) {
+      char* fmt_a = timer_to_str(a);
+      char* fmt_b = timer_to_str(b);
+
+      printf("a ==> %s \tb ==> %s\n", fmt_a, fmt_b);
+      free(fmt_b); free(fmt_a);
+
+		Sleep(700);
+   }
+   // Após o esgotamento:
+   char* fmt_a = timer_to_str(a);
+   char* fmt_b = timer_to_str(b);
+   printf("a ==> %s \tb ==> %s\n", fmt_a, fmt_b);
+   free(fmt_b); free(fmt_a);
+
+   destroi_temporizador(a);
+   destroi_temporizador(b);
+}
+
+void reutilizacao_do_temporizador(void) {
+	CABECALHO_DA_FUNCAO();
+
+   Timer obj = cria_temporizador(Segundo, 5);
+   bool temporizador_esta_esgotado = esgotado(obj);
+
+   assert (obj->reutilizacoes == 0);
+   do {
+      temporizador_esta_esgotado = esgotado(obj);
+
+      char* fmt = timer_to_str(obj);
+      printf("%s\n", fmt);
+      free(fmt);
+
+		Sleep(800);
+   } while(!temporizador_esta_esgotado);
+
+   recomecar(obj);
+   assert (obj->reutilizacoes == 1);
+   do {
+      temporizador_esta_esgotado = esgotado(obj);
+
+      char* fmt = timer_to_str(obj);
+      printf("%s\n", fmt);
+      free(fmt);
+
+		Sleep(800);
+   } while(!temporizador_esta_esgotado);
+
+   recomecar(obj);
+   assert (obj->reutilizacoes == 2);
+   do {
+      temporizador_esta_esgotado = esgotado(obj);
+
+      char* fmt = timer_to_str(obj);
+      printf("%s\n", fmt);
+      free(fmt);
+
+		Sleep(800);
+   } while(!temporizador_esta_esgotado);
+   destroi_temporizador(obj);
+}
+
+void disparando_o_cronometro(void) {
+	CABECALHO_DA_FUNCAO();
+
+   Cronometro tool = cria_cronometro();
+   double entrada[] = {3.51, 5.81, 7.23, 8.01, 14.2};
+   size_t cursor = 0;
+   
+   while(cursor < 5) {
+      // Registra a variação:
+      marca(tool);
+      // Impressão:
+      char* fmt = cronometro_to_str(tool);
+      printf("%s \t ~%3.2lfseg\n", fmt, entrada[cursor]);
+      free(fmt);
+      // Pausa deterministica:
+      // sleep((int)entrada[cursor++]);
+		Sleep((DWORD)entrada[cursor++]);
+   }
+   destroi_cronometro(tool);
+}
+#endif
 
 int main(int qtd, char* argumentos[], char* envp[]) 
 {
+	setlocale(LC_CTYPE, "en_US.UTF-8");
 	/* Os códigos estão sendo chamados de forma crua, porque a módulo que 
 	 * provém testes precisa deste módulo como depedência. */
    alocacao_e_desalocacao();
