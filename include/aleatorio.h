22079696ef8289c766b3b0d74a7b497099d00b4c
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2025-01-17 15:26:13 -0300

    embaralhador de array(qualquer tipo) adicionada

--- include/aleatorio.h
+++ include/aleatorio.h
@@ -2,21 +2,21 @@
  * o último tem um problema sério com as declarações de funções neste 
  * arquivo. */
 
+#ifndef ALEATORIO_H
+#define ALEATORIO_H
 #include <stddef.h>
 #include <stdbool.h>
-
-#ifndef _ALEATORIO_NOVO_H
-#define _ALEATORIO_NOVO_H
+#include "definicoes.h"
 
  /* Sorteia um inteiro positivo entre os limites dados(incluído o valor
   * superior). */
- size_t inteiro_positivo (size_t , size_t);
+ extern size_t inteiro_positivo (size_t , size_t);
 
  // Sorteia uma letra do alfabeto, seja ela minúscula ou maiúscula.
  unsigned char alfabeto_aleatorio (void);
 
  // Sorteia um valor lógico de maneira randômica.
- bool logico (void);
+ extern bool logico (void);
 
  // Sorteia um caractére(imprível) da tabela ASCII.
  char ascii_char_aleatorio (void);
@@ -24,7 +24,11 @@
  #ifdef __linux__
  /* Sorteia uma palavra randômica do dicionário, este geralmente nativo do
   * sistema operacional. */
- char* palavra_aleatoria(void);
+ char* palavra_aleatoria (void);
  #endif
+  
+ /* Embaralha arrays de qualquer tipo, claro, dado seu tamanho e tamanho
+  * em bytes do tipo. */
+ void embaralha_array(generico_t seq, int n, int sz);
 
-#endif // _ALEATORIO_NOVO_H
+#endif // ALEATORIO_H
