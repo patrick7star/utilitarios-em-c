75dbd1d2dcfcbb8260b1b9e4100039e9b098f079
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-03-15 10:00:59 -0300

    correção, faltando o declaração do `valor_legivel_double`

--- include/legivel.h
+++ include/legivel.h
@@ -20,15 +20,16 @@
  char* valor_legivel_usize (size_t); 
  char* valor_legivel_isize (int64_t);
  char* valor_legivel_f32   (float);
+ char* valor_legivel_f64   (double);
 
  // Função macro que aborda todos tipos de entradas:
  #define valor_legivel(U) _Generic((U),      \
-   int64_t: valor_legivel_isize,             \
-   int: valor_legivel_isize,                 \
-   size_t: valor_legivel_usize,              \
+   int64_t:  valor_legivel_isize,            \
+   int:      valor_legivel_isize,            \
+   size_t:   valor_legivel_usize,            \
    uint32_t: valor_legivel_usize,            \
-   float: valor_legivel_f32,                 \
-   double: valor_legivel_f64                 \
+   float:    valor_legivel_f32,              \
+   double:   valor_legivel_f64               \
  )(U)
 
  // Converte o tempo em segundos para uma forma mais legível.
@@ -37,11 +38,11 @@
  char* tempo_legivel_timespec (struct timespec t);
  char* tempo_legivel_timeval  (struct timeval t);
 
-#define tempo_legivel(T) _Generic((T), \
-   struct timespec: tempo_legivel_timespec, \
-   struct timeval: tempo_legivel_timeval, \
-   double: tempo_legivel_double, \
-   size_t: tempo_legivel_usize \
+#define tempo_legivel(T) _Generic((T),       \
+   struct timespec: tempo_legivel_timespec,  \
+   struct timeval: tempo_legivel_timeval,    \
+   double:         tempo_legivel_double,     \
+   size_t:         tempo_legivel_usize       \
 )(T)
 
 #endif //LEGIVEL_H
