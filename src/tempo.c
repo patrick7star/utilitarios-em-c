be140a8b445c1d2c0133f98d1fa7041b1abc50cc
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-06-21 13:48:42 -0300

    Processo de organização do módulo, e compatibilização com Windows

--- src/tempo.c
+++ src/tempo.c
@@ -1,17 +1,18 @@
-
-
 /* ferramentas importante de tempo como Cronometro e Temporizador. Os
  * "métodos" dela serão um pouco diferente e limitada comparada outras 
  * linguagens, pois o C não é uma linguagem OO. 
  */
 
+// Biblioteca padrão do C:
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
 #include <inttypes.h>
-#include <threads.h>
 #include <stdio.h>
+// Definições das funções abaixo:
 #include "tempo.h"
+// Biblioteca própria do Linux:
+#include <threads.h>
 
 
 /* Pausa a thread principal é extremamente complicado de se fazer
@@ -76,9 +77,9 @@ void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd)
 #include "tempo/cronometro.c"   // primeiro
 #ifdef __linux__
 #include <threads.h>
-#include "tempo/temporizador.c" // segundo.
+#include "tempo/temporizador.c"
 #elif defined(_WIN32)
-#include "tempo/temporizador-win.c" // segundo.
+#include "tempo/win/temporizador.c"
 #endif
 
 
@@ -87,15 +88,10 @@ void breve_pausa(TEMPO_TIPO tipo, uint16_t qtd)
  *                       Testes Unitários 
  *
  * --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---*/
-#if defined(_UT_TEMPO)
-#include <assert.h>
-#ifdef __linux__
+#if defined(_UT_TEMPO) && defined(__linux__)
 #include <unistd.h>
-#elif defined(_WIN32)
-#include <locale.h>
-#endif
+#include <assert.h>
 
-#ifdef __linux__
 void alocacao_e_desalocacao() {
    Cronometro c = cria_cronometro();
    // pausa.
@@ -273,17 +269,18 @@ void teste_desconhecido_ii(void) {
    size_t decorrido_nano = fim.tv_nsec - inicio.tv_nsec;
    printf("%ldseg %ldns\n", decorrido_seg, decorrido_nano);
 }
-#elif defined(_WIN32)
+#elif defined(_WIN32) && defined(__unit_tests__)
 #include <synchapi.h>
+#include <locale.h>
 
-#define CABECALHO_DA_FUNCAO(void) ({\
-	printf( \
-		"\n\n[linha: %d] Função em execução agora: '%s'\n", \
-		__LINE__, __FUNCTION__ \
-	);\
+#define CABECALHO_DA_FUNCAO(void) ({								\
+	printf(																	\
+		"\n\n[linha: %d] Função em execução agora: '%s'\n",	\
+		__LINE__, __FUNCTION__											\
+	);																			\
 })
 
-void alocacao_e_desalocacao() {
+void alocacao_e_desalocacao(void) {
 	CABECALHO_DA_FUNCAO();
    Cronometro c = cria_cronometro();
    // pausa.
@@ -295,7 +292,7 @@ void alocacao_e_desalocacao() {
    destroi_cronometro(c1);
 }
 
-void visualizacao_dos_marcos() {
+void visualizacao_dos_marcos(void) {
 	CABECALHO_DA_FUNCAO();
 
    Cronometro clock = cria_cronometro();
@@ -312,114 +309,6 @@ void visualizacao_dos_marcos() {
    // invocando a função que faz a listagem.
    puts("\nregistros feitos:");
    visualiza_marcos(clock);
-}
-
-void stringficacao_do_temporizador() {
-	CABECALHO_DA_FUNCAO();
-
-   Temporizador timer = cria_temporizador(Miliseg, 20);
-   assert(timer->reutilizacoes == 0);
-
-	Sleep(500);
-   puts(timer_to_str(timer));
-
-	Sleep(6);
-   puts(timer_to_str(timer));
-
-   recomecar(timer);
-   assert(timer->reutilizacoes == 1);
-
-	Sleep(3);
-   puts(timer_to_str(timer));
-
-	Sleep(12);
-   puts(timer_to_str(timer));
-
-	Sleep(8);
-   puts(timer_to_str(timer));
-
-   destroi_temporizador(timer);
-
-   Temporizador timer_seg = cria_temporizador(Segundo, 8);
-
-	Sleep(3000);
-   puts(timer_to_str(timer_seg));
-
-	Sleep(4000);
-   puts(timer_to_str(timer_seg));
-
-	Sleep(2000);
-   puts(timer_to_str(timer_seg));
-
-   destroi_temporizador(timer_seg);
-}
-
-void dois_tais_ate_o_esgotamento(void) {
-	CABECALHO_DA_FUNCAO();
-
-   Temporizador a = cria_temporizador(Segundo, 8);
-   Temporizador b = cria_temporizador(Segundo, 13);
-
-   while (!(esgotado(a) && esgotado(b))) {
-      char* fmt_a = timer_to_str(a);
-      char* fmt_b = timer_to_str(b);
-
-      printf("a ==> %s \tb ==> %s\n", fmt_a, fmt_b);
-      free(fmt_b); free(fmt_a);
-
-		Sleep(700);
-   }
-   // Após o esgotamento:
-   char* fmt_a = timer_to_str(a);
-   char* fmt_b = timer_to_str(b);
-   printf("a ==> %s \tb ==> %s\n", fmt_a, fmt_b);
-   free(fmt_b); free(fmt_a);
-
-   destroi_temporizador(a);
-   destroi_temporizador(b);
-}
-
-void reutilizacao_do_temporizador(void) {
-	CABECALHO_DA_FUNCAO();
-
-   Timer obj = cria_temporizador(Segundo, 5);
-   bool temporizador_esta_esgotado = esgotado(obj);
-
-   assert (obj->reutilizacoes == 0);
-   do {
-      temporizador_esta_esgotado = esgotado(obj);
-
-      char* fmt = timer_to_str(obj);
-      printf("%s\n", fmt);
-      free(fmt);
-
-		Sleep(800);
-   } while(!temporizador_esta_esgotado);
-
-   recomecar(obj);
-   assert (obj->reutilizacoes == 1);
-   do {
-      temporizador_esta_esgotado = esgotado(obj);
-
-      char* fmt = timer_to_str(obj);
-      printf("%s\n", fmt);
-      free(fmt);
-
-		Sleep(800);
-   } while(!temporizador_esta_esgotado);
-
-   recomecar(obj);
-   assert (obj->reutilizacoes == 2);
-   do {
-      temporizador_esta_esgotado = esgotado(obj);
-
-      char* fmt = timer_to_str(obj);
-      printf("%s\n", fmt);
-      free(fmt);
-
-		Sleep(800);
-   } while(!temporizador_esta_esgotado);
-   destroi_temporizador(obj);
 }
 
 void disparando_o_cronometro(void) {
@@ -442,13 +331,10 @@ void disparando_o_cronometro(void) {
    }
    destroi_cronometro(tool);
 }
-#endif
 
-int main(int qtd, char* argumentos[], char* envp[]) 
+int main(int qtd, char* argumentos[], char* envs[]) 
 {
-   #ifdef _WIN32
 	setlocale(LC_CTYPE, "en_US.UTF-8");
-   #endif
 
 	/* Os códigos estão sendo chamados de forma crua, porque a módulo que 
 	 * provém testes precisa deste módulo como depedência. */
