fe120f70c0cf94f3110bbe872e2d352b9939a142
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-06-22 01:38:30 -0300

    update do Makefile do Windows
    Aproveitando também, adicionando alguns scripts em Python(a linguagem é bem conveniente pra mim) para mexer nos múltiplos makefiles, de diferentes plataformas, sem falar em outras funções que vem pela frente.

---
+++ script/unico_makefile.py
@@ -0,0 +1,9 @@
+"""
+   Como a compatibilidade deste código com Windows, precisa meio que outras
+ diretizes de compilação -- a maioria está incompleta no momento que escrevo
+ este script; dois makefiles são escritos. O que fica um pouco chato quando
+ quero usar o do Windows, pois ele tem um nome maior, então preciso escrever
+ um comando com a opção de trocar o makefile, ou seja, não apenas 'make' 
+ para executar. Explicado isso, este script tem como função alternar os 
+ arquivos de maneira que, o "padrão", adeque-se com o sistema em execução.
+"""
