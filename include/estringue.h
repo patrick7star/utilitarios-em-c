e8567a4d3373530ca7cc682b41c52522d7e688dd
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-06-25 23:53:19 -0300

    mais funções adicionadas.
    Os métodos de 'Lista Strings' também estão começando a ficarem públicos.

--- include/estringue.h
+++ include/estringue.h
@@ -10,14 +10,21 @@
  *             uso da equivalente em inglẽs.
  */
 
-#ifndef ESTRINGUE_H
-#define ESTRINGUE_H
+#ifndef __ESTRINGUE_H__
+#define __ESTRINGUE_H__
 #include <wchar.h>
 #include <stdbool.h>
 
  // Tupla contendo array de strings e a quantia disto.
- typedef struct { char** lista; size_t total; } ListaStrings;
+ typedef struct { char** lista; size_t total; } ListaStrings, ListStr;
  typedef char* char_t, CHAR, Char;
+
+ /* Atenção: veja se a lista que você quer liberar, todos itens são alocação
+  * dinâmica, caso contrário, o programa será interrompido por corrupção
+  * de memória. */
+ void free_lista_strings(ListaStrings*); 
+ /* Visualização debug da tupla-lista de strings. */
+ void debug_lista_strings(ListaStrings*);
 
  /* Uma representação de string já codificada como Unicode. */
  typedef struct string_dinamica { 
@@ -35,14 +42,15 @@
  // retorna todas strings representando palavras(entre espaços).
  ListaStrings palavras(char*); 
  // retorna tupla com strings entre quebra-de-linhas.
- ListaStrings split_linhas(char*);
- ListaStrings split_at(char*, const char PADRAO);
- /* Atenção: veja se a lista que você quer liberar, todos itens são alocação
-  * dinâmica, caso contrário, o programa será interrompido por corrupção
-  * de memória. */
- void free_lista_strings(ListaStrings*); 
+ ListStr split_lines             (const char* In);
+ ListStr split_at                (const char* In, const char PADRAO);
+ ListStr split_ascii_whitespace  (const char* In);
+ ListStr split_once              (const char* In, const char PADRAO);
 
 
+/* == == == == == == == == == == == == == == == == == == == == == == == == ==
+ *                      Estrutura Mutável String
+ * == == == == == == == == == == == == == == == == == == == == == == === = */
  /* Operações em cima da estrutura String, sendo que elas podem ou não,
   * modificar o interno da estrutura. */
  String cria_com_capacidade_str (size_t);
@@ -62,10 +70,10 @@
  // Quantos caractéres da 'capacidade' ainda há.
  size_t vacuo_str(String);
 
-/* == == == == == == == == == == == == == == == == == == == == == == == == 
+/* == == == == == == == == == == == == == == == == == == == == == == == == ==
  *                      Funções Auxiliares para
  *                         Strings Puras em C
- * == == == == == == == == == == == == == == == == == == == == == == === */
+ * == == == == == == == == == == == == == == == == == == == == == == === = */
  char* maiuscula_ascii        (char* In);
  char* minuscula_ascii        (char* In);
  char* capitaliza_ascii       (char* In);
@@ -76,7 +84,7 @@
   * padrão(p) dado. 
   *
   * Nota:a lista de retorno são cópias.*/
- ListaStrings reparte_ascii   (char* s, char* p);
+ ListStr reparte_ascii   (char* s, char* p);
 
  /* Pega um caractére ASCII qualquer, e forma uma estringue com ele repetido
   * 'qtd' vezes. */
