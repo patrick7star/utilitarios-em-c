e357f23bafab970ba23af81b17edc8910c606655
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-10-26 12:31:08 -0300

    pequenas modificações prá permitir que ...
    multiplas plataformas utilizar a biblioteca.

--- src/teste.c
+++ src/teste.c
@@ -18,49 +18,50 @@
 #include "terminal.h"
 
 
-#ifdef __linux__
-/*   Primeiro suite de testes criados. Na verdade é um monte de funções, 
+#if defined(__linux__)
+/*   Primeiro suite de testes criados. Na verdade é um monte de funções,
  * baseado em uma única funçõa base. Difícilmente pode se descontinuada,
  * porque foi amplamente utilizada, e ainda é enquanto escrevo isso, apesar
  * de está com os dias contados.
  *   Foi passada para um próprio arquivo, por está dispersada pelo código,
  * sem falar que é uma das features com mais linhas, e até a mais importante
- * deste módulo(e de ele ser tão usado). 
- *    
+ * deste módulo(e de ele ser tão usado).
+ *
  *    Funções:
  *       - nome_do_teste
- *       - executa_testes 
+ *       - executa_testes
  *       - executa_testes_a
- *       - executa_teste_interruptor 
+ *       - executa_teste_interruptor
  *
  *    Variável:
  *       - testes_contagem
  */
   #include "teste/tester.c"
+
 #elif defined(_WIN32)
 /*   O único teste que tal módulo entrega é o 'executa_testes'. Bastante
- * simples, e cuida dos demais subcasos. Este é o padrão do trabalho de 
+ * simples, e cuida dos demais subcasos. Este é o padrão do trabalho de
  * compatibilidade com o Windows, apenas descontinuo antigas versões muito
- * rápido. Eles ficam mais minimalistas e  fáceis de arrumar. Claro, a 
+ * rápido. Eles ficam mais minimalistas e  fáceis de arrumar. Claro, a
  * desvantagem é que não tem tantas features, tão rápido. E, enquanto não
  * terminar de criar a versão para plataforma ele fica inutilizado, já que
  * não tem nunca versões legadas desta.
- * 
- *       - executa_testes 
+ *
+ *       - executa_testes
  */
   #include "teste/windows/tester.c"
 #endif
 
 // conta quantos usos a função abaixo tem.
 static uint8_t contagem_de_usos = 0;
-/* ótima ferramenta de 'debug', onde indica a linha e o arquivo onde 
+/* ótima ferramenta de 'debug', onde indica a linha e o arquivo onde
  * possível erro pode está. */
-void debug_aqui(void) {
+CROSSLIB void debug_aqui(void) {
    printf("\no erro está bem ... ... ...aqui!(%d)\n", contagem_de_usos + 1);
    contagem_de_usos++;
 }
 
-/* Função e seus protótipos para converter uma string que representa um 
+/* Função e seus protótipos para converter uma string que representa um
  * valor lógica, seja qualquer a forma que ele está escrito, no respectivo
  * valor lógico que representa. As funções auxiliares a função principal(
  * str_to_bool) também é listada, pois alguns testes fazem uso delas.
@@ -76,11 +77,11 @@ void debug_aqui(void) {
 #if defined(__unit_tests__) && defined(__linux__)
 /* === === === === === === === === === === === === === === === === === ===+
  *                         Testes Unitários
- * 
+ *
  *   Testando todos estrutura, métodos e funções declarados e implementados
  * acima. Esta parte abaixo pode futuramente ser colocada em outra arquivo
  * e incluído aqui, não faz a menor diferença, e por cima deixa tal arquivo
- * mais limpo. 
+ * mais limpo.
  * === === === === === === === === === === === === === === === === === ===*/
 void converte_strings_de_valores_logicos() {
    struct par {
@@ -89,10 +90,10 @@ void converte_strings_de_valores_logicos() {
    };
 
    struct par matches[] = {
-      {"verdadeiro", true }, { "true", true }, { "falso", false }, 
-      { "false", false }, { "FALSO", false }, { "FALse", false }, 
-      { "TRUE", true }, { "vErDaDeiRO", true }, { "Falso", false }, 
-      { "TrUe", true }, { "True", true }, {"T", true}, {"V", true}, 
+      {"verdadeiro", true }, { "true", true }, { "falso", false },
+      { "false", false }, { "FALSO", false }, { "FALse", false },
+      { "TRUE", true }, { "vErDaDeiRO", true }, { "Falso", false },
+      { "TrUe", true }, { "True", true }, {"T", true}, {"V", true},
       {"f", false}, {"F", false}
    };
 
@@ -100,7 +101,7 @@ void converte_strings_de_valores_logicos() {
       char* e = matches[p].entrada;
       bool s = matches[p].saida;
       printf(
-         "%luº)\t%-13s==>%13s\n", 
+         "%luº)\t%-13s==>%13s\n",
          (p + 1), e, bool_to_str(s)
       );
       assert (str_to_bool(e) == s);
@@ -133,7 +134,7 @@ void primeiro_executor_datado(void) {
 
 int main(int qtd, char* argumentos[], char* env_vars[]) {
    executa_testes_a(
-      false, 4, 
+      false, 4,
          // iteração para gerar máscaras funciona!
          stringficacao_de_valores_primitivos, false,
          converte_strings_de_valores_logicos, false,
@@ -159,5 +160,5 @@ int main(int qtd, char* args[], char* vars[]) {
    captura_de_todos_argumentos();
    return EXIT_SUCCESS;
 }
-#endif 
+#endif
 
