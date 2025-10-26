e9a404121f08a547c8022b945b76b9e7e23483f7
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-10-26 12:39:28 -0300

    Agora, header também usada no Windows/MSVC
    Tanto ela quanto o 'legivel.h' podem te suas versões prá Windows deletadas. O problema principal ainda é como organizar o 'header file', já que ele é muito utilizado como consulta. E prá fazer tal base multiplataforma, várias coisas devem ser adicionadas, o que prejudica a leitura.

--- include/teste.h
+++ include/teste.h
@@ -1,12 +1,28 @@
-
+/**/
 
 #ifndef TESTE_H
 #define TESTE_H
 #include <stdbool.h>
 #include <stdint.h>
 
-// nome mais curto, estético e significativo para um ponteiro de
-// de função.
+/* Fazendo a biblioteca multiplataforma. Isso é importante aqui, pois faz
+ * tal lib também utilizável no Windows. */
+#if defined(_WIN32)
+  // Importantes definições para funcionar no Windows.
+  #define _USE_MATH_DEFINES
+
+  #ifdef __LIB_DLL__
+    #define CROSSLIB __declspec(dllexport)
+  #else
+    #define CROSSLIB __declspec(dllimport)
+  #endif
+
+#elif defined(__linux__)
+    #define CROSSLIB
+#endif
+
+ /* Nome mais curto, estético e significativo para um ponteiro de
+  * de função. */
  typedef void(*Fn)(void);
  // Tipo de função especial para testes-unitários.
  typedef int (*FnTst) (void);
@@ -16,20 +32,19 @@
  #define SEM_MENSAGEM ""
  #define SEM_NOME SEM_MENSAGEM
 
- extern void debug_aqui(void);
+ extern CROSSLIB void debug_aqui(void);
 
 /* Endereço de memória da função a executar; e sua definação,... se é para
  * executa-la?! */
 struct TesteConfig { const char* nome; Fn rotina; bool ativado; };
 
-#define UnitB(FUNCAO, ESTADO) (  \
+#define Unit(FUNCAO, ESTADO) (  \
    (struct TesteConfig){         \
       .nome = #FUNCAO,           \
       .rotina = FUNCAO,          \
       .ativado = ESTADO          \
    }                             \
 )
-#define Unit UnitB
 
 /* Executa variados testes passados. Os parâmetros seguem a seguinte
  * lógica: o total de argumentos totais, aí começa a alternância
@@ -45,11 +60,18 @@ struct TesteConfig { const char* nome; Fn rotina; bool ativado; };
  * inicialmente.
  *
  *   Cada nova geração ganha uma letra mais avançada no alfabeto, isso até
- * este tipo de teste for realmente substituído por algo mais avançado. */
-void executa_tst       (char*, Fn, bool);
-void executa_testes    (int total, ...);
-void executa_testes_a  (bool ativado, int total, ...);
-void executa_testes_b  (bool ativado, int total, ...);
+ * este tipo de teste for realmente substituído por algo mais avançado.
+
+ *   Na versão do Windows irei inovar, e sempre colocar apenas uma função
+ * que executa testes, portanto, sem as antigas versões. */
+ #if defined(_WIN32)
+ CROSSLIB void executa_testes  (bool ativado, int total, ...);
+ #elif defined(__linux__)
+ void executa_tst       (char*, Fn, bool);
+ void executa_testes    (int total, ...);
+ void executa_testes_a  (bool ativado, int total, ...);
+ void executa_testes_b  (bool ativado, int total, ...);
+ #endif
 
 /* Converte um valor lógico para uma string, em português, dizendo o que
  * ele representa. Nome similires, mas fazem a mesma coisas também estão
@@ -57,7 +79,21 @@ void executa_testes_b  (bool ativado, int total, ...);
  * exatamente o reverso, pega uma string que representa um valor verdade,
  * então retorna o valor-verdade em sí.
  */
- extern bool str_to_bool(char* s);
+ extern CROSSLIB bool str_to_bool(char* s);
 
+ /* O compilador nativo do Windows(MSVC) é muito chato com regras do ISO C.
+  * Por isso este macro está sendo colocado aqui. Gosto do nome, mas ainda
+  * não quero fazer multiplo plataforma. Ele pega o tipo de todo teste, um
+  * 'void', maís o qualificador 'static' exigido no 'clang-cl' prá toda
+  * função e variável que não é global. Como é prática, todos os nomes que
+  * podem ser pensados, poderão ser usados.
+  *   Usar isso nas plataformas Unix é totalmente opcional. Acho que é algo
+  * que deixa o código bem mais legível.
+  */
+ #define TESTE          static void
+ #define UNIT_TEST      TESTE
+ #define TESTE_UNITARIO TESTE
+ #define UNITARIO       TESTE
 
 #endif //TESTE_H
+
